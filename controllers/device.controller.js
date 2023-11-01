af1f5d4ec7dc555088432c7d6c330943d8cad22e
ESHWAR <chalamallan3@gmail.com>
2023-11-02 00:48:01 +0530

    device commit

--- controllers/device.controller.js
+++ controllers/device.controller.js
@@ -3,6 +3,20 @@ const router = express.Router();
 const Device = require('../models/Device.model');
 
 // Endpoint to update device state (light, fan, misc)
+
+router.post('/devices/create', async (req, res) => {
+    const { alloted_to_user } = req.body;
+  
+    try {
+      const newDevice = await Device.create({ alloted_to_user });
+      return res.status(201).json(newDevice);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  
 router.put('/devices/:deviceId', async (req, res) => {
   const { light, fan, misc } = req.body;
   try {
