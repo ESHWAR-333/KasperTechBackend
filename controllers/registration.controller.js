c9201b5d46fb2f7058465583ef8ee88831a32590
ESHWAR <chalamallan3@gmail.com>
2023-11-01 20:52:12 +0530

    first commit

--- controllers/registration.controller.js
+++ controllers/registration.controller.js
@@ -9,11 +9,39 @@ router.get('/',(req,res)=>{
     .catch(err=>console.log(err))
 })
 
-router.post('/insert',(req,res)=>{
-    Registartion.create(req.body)
-    .then(data=>res.send(data))
-    .catch(err=>console.log(err))
-})
+// router.post('/insert',(req,res)=>{
+//     Registartion.create(req.body)
+//     .then(data=>res.send(data))
+//     .catch(err=>console.log(err))
+// })
+
+
+router.post("//insert", async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+  
+    try {
+      const existingUser = await Registartion.findOne({ username });
+      if (existingUser) {
+        return res.status(400).send("User already exists");
+      }
+  
+      if (password.length < 6) {
+        return res.status(400).send("Password is too short");
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const newUser = new Registartion({
+        firstName, lastName, email, password: hashedPassword,
+      });
+      await newUser.save();
+  
+      res.status(201).send("User created successfully");
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  });
 
 module.exports=router
 
