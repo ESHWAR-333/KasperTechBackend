e49294cfb1a72b14469a54f7fa3d11cdfb036dfe
ESHWAR <chalamallan3@gmail.com>
2023-11-02 13:21:54 +0530

    device commit

--- controllers/device.controller.js
+++ controllers/device.controller.js
@@ -28,15 +28,18 @@ router.put('/devices/:deviceId', async (req, res) => {
   const { light, fan, misc } = req.body;
   const deviceId = req.params.deviceId;
   console.log('Received Device ID:', deviceId);
+
   try {
     const device = await Device.findByIdAndUpdate(
-     deviceId,
+      deviceId,
       { $set: { 'state.light': light, 'state.fan': fan, 'state.misc': misc } },
       { new: true }
     );
+
     if (!device) {
       return res.status(404).json({ error: 'Device not found' });
     }
+
     return res.status(200).json(device);
   } catch (error) {
     console.error(error);
